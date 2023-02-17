a45c79f57665ae5b83ff58b6661f48a4cb7dafef
TonyDev92 <tonydeveloper92@gmail.com>
2023-02-17 11:06:47 +0100

    Ejercicio 16 Javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+const boton1 = document.getElementById("b1");
+
+boton1.addEventListener("click" , () => {
+    alert("Click en el botón")
+})
+
+const boton2 = document.querySelector("#b2");
+
+boton2.addEventListener("click", () => {
+    alert("Hola, estoy utilizando JQuery")
+})
